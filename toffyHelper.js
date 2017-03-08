b862b8b1ebb4ded8e9e908d22a31ce9a188dc781
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-03-08 16:35:35 +0200

    toffyhelper]

--- toffyHelper.js
+++ toffyHelper.js
@@ -554,7 +554,7 @@ module.exports.getNewSession = function getNewSession(email, callback) {
         console.log("userIdInHr ====>>>" + userIdInHr);
 
         var cookies = JSON.stringify((response.headers["set-cookie"])[0]);
-
+        console.log("cookies==================>"+cookies)
         var arr = cookies.toString().split(";")
         console.log("trim based on ;==========>"+arr[0])
          arr = arr[0].toString().split("\*")
