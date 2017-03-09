45b86aea4783ce27258f7ded320c4f1c470c7d2e
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-03-09 10:28:33 +0200

    toffyhelper]

--- toffyHelper.js
+++ toffyHelper.js
@@ -668,7 +668,7 @@ module.exports.sendVacationPostRequest = function sendVacationPostRequest(from, 
         console.log("the vacation have been posted")
         console.log(error)
         console.log(response.statusCode)
-        console.log(JSON.stringify(body))
+        console.log((JSON.stringify(body)).id)
         console.log("Body .id"+body.id)
 
 
