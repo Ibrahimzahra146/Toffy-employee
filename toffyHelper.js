56a8fbcdeeea0fc8a42ef8f135f50f7ea2074d31
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-03-13 11:45:08 +0200

    send manager email

--- toffyHelper.js
+++ toffyHelper.js
@@ -633,7 +633,7 @@ module.exports.showHolidays = function showHolidays(msg, email, date, date1) {
                             console.log("i inside if" + stringMessage)
 
                         }
-                        stringMessage = stringMessage + "{" + "'title:'" + "\"" + (JSON.parse(body))[i].comments + "\"" + ",\"value\":" + "\"" + (JSON.parse(body))[i].fromDate + "\"" + ",\"short\":true}"
+                        stringMessage = stringMessage + "{" + "'title':" + "'" + (JSON.parse(body))[i].comments + "'" + ",'value':" + "'" + (JSON.parse(body))[i].fromDate + "'"; + ",'short':true}"
 
                         console.log("string" + i)
 
@@ -661,9 +661,9 @@ module.exports.showHolidays = function showHolidays(msg, email, date, date1) {
                     }
                     console.log("messageBody" + messageBody)
                     var stringfy = JSON.stringify(messageBody);
-                    console.log("stringfy  " + stringfy)
+                    var parsedMessage = JSON.parse(stringfy)
 
-                    msg.say(messageBody)
+                    msg.say(parsedMessage)
                 }
             })
 
