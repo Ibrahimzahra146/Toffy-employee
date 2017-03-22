d5faaa7264e15d7e94db2c93a859698677777389
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-03-22 15:22:43 +0200

    the name of holiday date

--- vacations.js
+++ vacations.js
@@ -1,5 +1,9 @@
+var request = require('request')
+var IP = process.env.SLACK_IP
+
+
 //send confirmation to emp when he request one day vacation 
-  module.exports.sendOneDaySickVacationConfirmationtoEmp=function sendOneDaySickVacationConfirmationtoEmp(msg, startDate, endDate, email, type) {
+module.exports.sendOneDaySickVacationConfirmationtoEmp = function sendOneDaySickVacationConfirmationtoEmp(msg, startDate, endDate, email, type) {
   msg.say("Sorry to hear that :( ")
   var text12 = {
     "text": "",
@@ -31,7 +35,7 @@
   msg.say(text12)
 }
 // send confirmation to employee when he want just one day vacation
-module.exports.sendOneDayVacationConfirmationtoEmp=function sendOneDayVacationConfirmationtoEmp(msg, startDate, endDate, email, type) {
+module.exports.sendOneDayVacationConfirmationtoEmp = function sendOneDayVacationConfirmationtoEmp(msg, startDate, endDate, email, type) {
   var text12 = {
     "text": "",
     "attachments": [
@@ -63,7 +67,7 @@ module.exports.sendOneDayVacationConfirmationtoEmp=function sendOneDayVacationCo
 }//****************************************************************************
 // send a confirmation to user to be sure he want the vacation and enter true dates
 
-module.exports.sendVacationConfirmationToEmp=function sendVacationConfirmationToEmp(msg, startDate, endDate, email, type) {
+module.exports.sendVacationConfirmationToEmp = function sendVacationConfirmationToEmp(msg, startDate, endDate, email, type) {
   var text12 = {
     "text": "",
     "attachments": [
@@ -91,6 +95,28 @@ module.exports.sendVacationConfirmationToEmp=function sendVacationConfirmationTo
       }
     ]
   }
-  
+
   msg.say(text12)
+}
+function getWorkingDays(startDate, endDate, email, callback) {
+  var vacationBody = {
+    "from": startDate,
+    "to": endDate
+
+  }
+  vacationBody = JSON.stringify(vacationBody)
+
+  request({
+    url: "http://" + IP + "/api/v1/vacation/working-days", //URL to hitDs
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Cookie': toffyHelper.generalCookies
+    },
+    body: vacationBody
+    //Set the body as a stringcc
+  }, function (error, response, body) {
+
+  })
+
 }
