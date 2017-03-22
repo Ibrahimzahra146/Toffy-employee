152efb33c5940a1b88b63f4b576fb3d168463439
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-03-22 15:38:46 +0200

    the name of holiday date

--- vacations.js
+++ vacations.js
@@ -121,10 +121,28 @@ function getWorkingDays(startDate, endDate, email, callback) {
     body: vacationBody
     //Set the body as a stringcc
   }, function (error, response, body) {
-    console.log("getWorkingDays" + response.statusCode)
-    console.log("getWorkingDays" + body);
-    console.log("getWorkingDays" + JSON.stringify(body));
-    callback(body)
+    if (response.statusCode == 403) {
+      toffyHelper.sessionFlag = 0;
+    } toffyHelper.getNewSession(email, function (cookies) {
+      toffyHelper.generalCookies = cookies
+      request({
+        url: "http://" + IP + "/api/v1/vacation/working-days", //URL to hitDs
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Cookie': toffyHelper.generalCookies
+        },
+        body: vacationBody
+        //Set the body as a stringcc
+      }, function (error, response, body) {
+        console.log("getWorkingDays" + response.statusCode)
+        console.log("getWorkingDays" + body);
+        console.log("getWorkingDays" + JSON.stringify(body));
+        callback(body)
+      })
+
+    })
+
 
 
   })
