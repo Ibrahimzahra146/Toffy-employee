4b7eaad8604205966d82ae66986caf84a970b8ee
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-04-16 16:30:33 +0300

    user message

--- server.js
+++ server.js
@@ -463,10 +463,10 @@ function sendRequestToApiAi(emailValue, msg) {
       var date;
       var date1;
 
-      if(!(response.result.parameters.date || response.result.parameters.date == "")){
+      if (!(response.result.parameters.date || response.result.parameters.date == "")) {
         console.log("not equal")
       }
-      if (response.result.parameters.holiday_synonymes && !(response.result.parameters.next_synonymes) && !(response.result.parameters.date || response.result.parameters.date != "") && !(response.result.parameters.date1) && !(response.result.parameters.number)) {
+      if (response.result.parameters.holiday_synonymes && !(response.result.parameters.next_synonymes) && !(response.result.parameters.date) && !(response.result.parameters.date != "") && !(response.result.parameters.date1) && !(response.result.parameters.number)) {
         console.log("1")
         date = "2017-01-01";
         date1 = "	2017-12-30";
