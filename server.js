c9a825d840abd6fb5bb0ff23a68219a6585dd19b
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-04-23 16:29:53 +0300

    update user messages

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ var controller2 = Botkit.slackbot({
   debug: true,
 });
 var employeeBot = controller2.spawn({
-  token: "s"//SLACK_EMPLOYEE_BOT_ACCESS_KEY
+  token: SLACK_EMPLOYEE_BOT_ACCESS_KEY
 
 }).startRTM();
 exports.employeeBot = employeeBot
