7b02579ec69b2f429eaeee55086708ef6214f4c2
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-04-30 11:48:47 +0300

    inageUrl

--- server.js
+++ server.js
@@ -608,7 +608,20 @@ slapp.message('(.*)', ['direct_message'], (msg, text, match1) => {
     var stringfy = JSON.stringify(msg);
     console.log("the message");
     console.log(stringfy);
-    getMembersList(msg.body.event.user, msg)
+    // getMembersList(msg.body.event.user, msg)
+    console.log("arrive")
+    request({
+      url: 'http://46.43.71.50:19090/api/v1/employee/login', //URL to hitDs
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: 'ibrahim.zahra@exalt.ps'
+    }, function (error, response, body) {
+      console.log("arrive")
+
+      console.log("getNewSessionwithCookie:" + response.statusCode)
+    });
 
 
   }
