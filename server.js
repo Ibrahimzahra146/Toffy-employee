cd0bb4408461238b03fff07a81acc036be394c92
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-05-07 11:07:58 +0300

    enhance org

--- server.js
+++ server.js
@@ -473,7 +473,7 @@ app.post('/birthday', (req, res) => {
 
 });
 app.post('/uploaded_sick_report', (req, res) => {
-  console.log(" New get request is received");
+  console.log(" New get request  is received");
   console.log(req.body)
   var parsedBody = JSON.parse(req.body)
   var vacationId = parsedBody.id
