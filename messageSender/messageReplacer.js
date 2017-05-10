6f120de6b9f8899c74a2f4648a4cdb2ce2374954
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-05-10 15:01:24 +0300

    enhance org

--- messageSender/messageReplacer.js
+++ messageSender/messageReplacer.js
@@ -13,83 +13,86 @@ module.exports.replaceWithComment = function replaceWithComment(msg, fromTime, t
         holidaysNotice = ""
     }
     var dont_detuct_button = ""
-
-    var messageBody = {
-        "text": "",
-        "attachments": [
-            {
-                "text": messageText + holidaysNotice,
-                "callback_id": 'leave_with_vacation_confirm_reject',
-                "color": "#3AA3E3",
-                "attachment_type": "default",
-                "actions": [
-                    {
-                        "name": 'Send_Commnet',
-                        "text": "Personal",
-
-                        "type": "button",
-                        "value": fromTime + ";" + toTime + ";" + email + ";" + fromMilliseconds + ";" + toMilliseconds + ";" + type + ";" + workingDays + ";" + wordFromDate + ";" + wordTodate + ";" + messageText + ";" + "Personal"
-                    },
-                    {
-                        "name": 'Send_Commnet',
-                        "text": "Travel",
-
-                        "type": "button",
-                        "value": fromTime + ";" + toTime + ";" + email + ";" + fromMilliseconds + ";" + toMilliseconds + ";" + type + ";" + workingDays + ";" + wordFromDate + ";" + wordTodate + ";" + messageText + ";" + "Travel"
-                    },
+    getComment(workingDays, type, function (comment1, comment2, comment3, comment4f) {
 
 
+        var messageBody = {
+            "text": "",
+            "attachments": [
+                {
+                    "text": messageText + holidaysNotice,
+                    "callback_id": 'leave_with_vacation_confirm_reject',
+                    "color": "#3AA3E3",
+                    "attachment_type": "default",
+                    "actions": [
+                        {
+                            "name": 'Send_Commnet',
+                            "text": comment1,
 
-                    {
-                        "name": 'Send_Commnet',
-                        "text": "Umrah",
+                            "type": "button",
+                            "value": fromTime + ";" + toTime + ";" + email + ";" + fromMilliseconds + ";" + toMilliseconds + ";" + type + ";" + workingDays + ";" + wordFromDate + ";" + wordTodate + ";" + messageText + ";" + comment1
+                        },
+                        {
+                            "name": 'Send_Commnet',
+                            "text": comment2,
 
-                        "type": "button",
-                        "value": fromTime + ";" + toTime + ";" + email + ";" + fromMilliseconds + ";" + toMilliseconds + ";" + type + ";" + workingDays + ";" + wordFromDate + ";" + wordTodate + ";" + messageText + ";" + "Umrah"
-                    },
-                    {
-                        "name": 'Send_Commnet',
-                        "text": "Honeymoon",
-
-                        "type": "button",
-                        "value": fromTime + ";" + toTime + ";" + email + ";" + fromMilliseconds + ";" + toMilliseconds + ";" + type + ";" + workingDays + ";" + wordFromDate + ";" + wordTodate + ";" + messageText + ";" + "Honeymoon"
-                    },
-
-                    {
-                        "name": 'Undo',
-                        "text": "back",
-
-                        "style": "danger",
-                        "type": "button",
-                        "value": fromTime + ";" + toTime + ";" + email + ";" + fromMilliseconds + ";" + toMilliseconds + ";" + type + ";" + workingDays + ";" + wordFromDate + ";" + wordTodate + ";" + messageText + ";" + "Honeymoon"
-                    },
-
-                    {
-                        "name": 'Send_Commnet',
-                        "text": "Funeral",
-
-                        "type": "button",
-                        "value": fromTime + "," + toTime + "," + email + "," + fromMilliseconds + "," + toMilliseconds + "," + type + "," + workingDays + "," + wordFromDate + "," + wordTodate + "," + messageText + ";" + "Umrah"
-                    }
-                    ,
-                    {
-                        "name": 'Send_Commnet',
-                        "text": "Family affairs",
-
-                        "type": "button",
-                        "value": fromTime + "," + toTime + "," + email + "," + fromMilliseconds + "," + toMilliseconds + "," + type + "," + workingDays + "," + wordFromDate + "," + wordTodate + "," + messageText + ";" + "Umrah"
-                    }
+                            "type": "button",
+                            "value": fromTime + ";" + toTime + ";" + email + ";" + fromMilliseconds + ";" + toMilliseconds + ";" + type + ";" + workingDays + ";" + wordFromDate + ";" + wordTodate + ";" + messageText + ";" + comment2
+                        },
 
 
 
+                        {
+                            "name": 'Send_Commnet',
+                            "text": comment3,
 
-                ],
-                "color": "#F35A00",
+                            "type": "button",
+                            "value": fromTime + ";" + toTime + ";" + email + ";" + fromMilliseconds + ";" + toMilliseconds + ";" + type + ";" + workingDays + ";" + wordFromDate + ";" + wordTodate + ";" + messageText + ";" + comment3
+                        },
+                        {
+                            "name": 'Send_Commnet',
+                            "text": comment4,
 
-            }
-        ]
-    }
-    msg.respond(msg.body.response_url, messageBody)
+                            "type": "button",
+                            "value": fromTime + ";" + toTime + ";" + email + ";" + fromMilliseconds + ";" + toMilliseconds + ";" + type + ";" + workingDays + ";" + wordFromDate + ";" + wordTodate + ";" + messageText + ";" + comment4
+                        },
+
+                        {
+                            "name": 'Undo',
+                            "text": "back",
+
+                            "style": "danger",
+                            "type": "button",
+                            "value": fromTime + ";" + toTime + ";" + email + ";" + fromMilliseconds + ";" + toMilliseconds + ";" + type + ";" + workingDays + ";" + wordFromDate + ";" + wordTodate + ";" + messageText + ";" + "Honeymoon"
+                        },
+
+                        {
+                            "name": 'Send_Commnet',
+                            "text": "Funeral",
+
+                            "type": "button",
+                            "value": fromTime + "," + toTime + "," + email + "," + fromMilliseconds + "," + toMilliseconds + "," + type + "," + workingDays + "," + wordFromDate + "," + wordTodate + "," + messageText + ";" + "Umrah"
+                        }
+                        ,
+                        {
+                            "name": 'Send_Commnet',
+                            "text": "Family affairs",
+
+                            "type": "button",
+                            "value": fromTime + "," + toTime + "," + email + "," + fromMilliseconds + "," + toMilliseconds + "," + type + "," + workingDays + "," + wordFromDate + "," + wordTodate + "," + messageText + ";" + "Umrah"
+                        }
+
+
+
+
+                    ],
+                    "color": "#F35A00",
+
+                }
+            ]
+        }
+        msg.respond(msg.body.response_url, messageBody)
+    })
 }
 //Undo in the employee side 
 module.exports.undoUserComment = function undoUserComment(msg, fromTime, toTime, email, fromMilliseconds, toMilliseconds, type, workingDays, wordFromDate, wordTodate, messagetext) {
@@ -135,5 +138,18 @@ module.exports.undoUserComment = function undoUserComment(msg, fromTime, toTime,
         ]
     }
     msg.respond(msg.body.response_url, messageBody)
+
+}
+//Get comment based on type and period
+function getComment(workingDays, type, callback) {
+    if (type == "sick") {
+        callback("Personal", "Flu", "Common cold", "Surgery")
+    } else if (workingDays < 1) {
+        callback("Personal", "Something urgent", "Traffic", "Bad weather")
+    } else if (workingDays >= 1 && workingDays <= 3) {
+        callback("Personal", "Family illness", "University", "Travel")
+    } else if (workingDays > 3) {
+        callback("Personal", "Travel", "Umrah", "Honeymoon")
+    }
 
 }
