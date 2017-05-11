e3e9c704e630d4dbec1ce3cbd5aaa1522c1dcd51
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-05-11 11:36:46 +0300

    enhance org

--- toffyHelper.js
+++ toffyHelper.js
@@ -278,7 +278,14 @@ module.exports.sendVacationToManager = function sendVacationToManager(startDate,
                                             "style": "danger",
                                             "type": "button",
                                             "value": userEmail + ";" + vacationId + ";" + approvalId + ";" + managerEmail + ";employee" + ";" + startDate + ";" + endDate + ";" + type + ";" + workingDays + ";" + ImageUrl
-                                        }, dont_detuct_button
+                                        }, dont_detuct_button,
+                                        {
+                                            "name": "check_state",
+                                            "text": "Check status",
+
+                                            "type": "button",
+                                            "value": userEmail + ";" + vacationId + ";" + approvalId + ";" + managerEmail + ";employee" + ";" + startDate + ";" + endDate + ";" + type + ";" + workingDays + ";" + ImageUrl
+                                        },
                                     ],
                                     "color": "#F35A00",
                                     "thumb_url": ImageUrl,
