f43f31042c7dbd9e132a2dc48c67cc43b59ad192
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-05-15 13:16:09 +0300

    add new fields

--- server.js
+++ server.js
@@ -524,7 +524,7 @@ slapp.action('preDefinedHelp', 'Show_stats', (msg, value) => {
 // console.log(""+JSON.parse(msg))
  // var email=SendWelcomeResponse(JSON.parse(msg),"",1)
   //console.log("SHOW stats listenerr" + email)
-  console.log(msg)
+  console.log(msg.body.user.id)
  // console.log(JSON.stringify(msg))
  // console.log(JSON.stringify(JSON.parse(msg)))
   msg.say("help")
