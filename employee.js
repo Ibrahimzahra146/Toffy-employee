6450d3dd38d40a1cb444b02b74bebd8741954e81
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-05-15 10:40:37 +0300

    add new fields

--- employee.js
+++ employee.js
@@ -7,7 +7,7 @@ var IP = process.env.SLACK_IP
 var userIdInHr = "initial";
 exports.userIdInHr = userIdInHr
 var toffyHelper = require('./toffyHelper')
-
+const stringFile = require('./strings.js')
 var async = require('async');
 var currentBot = server.bot;
 var hrRole = 0;
@@ -16,6 +16,7 @@ var remember_me = "initial";
 /****** 
 Show employee vacation history
 ******/
+
 module.exports.showEmployeeHistory = function showEmployeeHistory(email, msg) {
 
     printLogs("Show employee history")
@@ -330,91 +331,18 @@ module.exports.sendHelpOptions = function sendHelpOptions(msg) {
                 "pretext": "You can use on of the following expressions to engage with me:",
                 "color": "#3AA3E3",
                 "attachment_type": "default",
-                "fields": [
-                    {
-                        "title": "Time off tomorrow ",
-                        "value": "",
-                        "short": false
-                    },
-                    {
-                        "title": "Time off or vacation from 3 may to 5 may ",
-                        "value": "",
-                        "short": false
-                    },
-                    {
-                        "title": "I want a vacation or time off tomorrow or next Monday  ",
-                        "value": "",
-                        "short": false
-                    },
-                    {
-                        "title": "I am sick today or i was sick yesterday ",
-                        "value": "",
-                        "short": false
-                    },
-                    {
-                        "title": "I want a maternity time off from 20 May",
-                        "value": "",
-                        "short": false
-                    }
-                    ,
-                    {
-                        "title": "Paternity time off on 10 May",
-                        "value": "",
-                        "short": false
-                    },
-                    {
-                        "title": "Marriage vacation on 10 May",
-                        "value": "",
-                        "short": false
-                    }
-                    ,
-                    {
-                        "title": "Haj vacation on 10 June ",
-                        "value": "",
-                        "short": false
-                    }
-                    ,
-                    {
-                        "title": "Family death today or yesterday ",
-                        "value": "",
-                        "short": false
-                    }
-
-                    ,
-                    {
-                        "title": "Show holidays or next holidays ",
-                        "value": "",
-                        "short": false
-                    }
-                    ,
-                    {
-                        "title": "Show time off submission rules ",
-                        "value": "",
-                        "short": false
-                    }
-                    ,
-                    {
-                        "title": "Work from home today or WFH from 5 May to 8 May ",
-                        "value": "",
-                        "short": false
-                    }
-                    ,
-                    {
-                        "title": "Tutorial video https://www.screencast.com/t/pPR0xftK",
-                        "value": "",
-                        "short": false
-                    }
-                ], "actions": [
+                "fields": stringFile.staticHelpFields,
+                "actions": [
                     {
                         "name": 'Show_stats',
                         "text": "Show stats",
-                       // "style": "primary",
+                        // "style": "primary",
                         "type": "button",
                         "value": ""
                     }, {
                         "name": 'Show_profile',
                         "text": "Show profile",
-                       // "style": "primary",
+                        // "style": "primary",
                         "type": "button",
                         "value": ""
                     }, {
---
+++ strings.js
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+var staticHelpFields = 
+ [
+        {
+            "title": "Time off tomorrow ",
+            "value": "",
+            "short": false
+        },
+        {
+            "title": "Time off or vacation from 3 may to 5 may ",
+            "value": "",
+            "short": false
+        },
+        {
+            "title": "I want a vacation or time off tomorrow or next Monday  ",
+            "value": "",
+            "short": false
+        },
+        {
+            "title": "I am sick today or i was sick yesterday ",
+            "value": "",
+            "short": false
+        },
+        {
+            "title": "I want a maternity time off from 20 May",
+            "value": "",
+            "short": false
+        }
+        ,
+        {
+            "title": "Paternity time off on 10 May",
+            "value": "",
+            "short": false
+        },
+        {
+            "title": "Marriage vacation on 10 May",
+            "value": "",
+            "short": false
+        }
+        ,
+        {
+            "title": "Haj vacation on 10 June ",
+            "value": "",
+            "short": false
+        }
+        ,
+        {
+            "title": "Family death today or yesterday ",
+            "value": "",
+            "short": false
+        }
+
+        ,
+        {
+            "title": "Show holidays or next holidays ",
+            "value": "",
+            "short": false
+        }
+        ,
+        {
+            "title": "Show time off submission rules ",
+            "value": "",
+            "short": false
+        }
+        ,
+        {
+            "title": "Work from home today or WFH from 5 May to 8 May ",
+            "value": "",
+            "short": false
+        }
+        ,
+        {
+            "title": "Tutorial video https://www.screencast.com/t/pPR0xftK",
+            "value": "",
+            "short": false
+        }
+    ]
+
+exports.staticHelpFields = staticHelpFields;
