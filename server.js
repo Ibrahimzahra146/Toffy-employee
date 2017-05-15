79cdf62a8c00e55b990d07ed347990c0f1dc46a8
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-05-15 13:25:39 +0300

    add new fields

--- server.js
+++ server.js
@@ -97,14 +97,11 @@ function SendWelcomeResponse(msg, responseText, flag, callback) {
     id = msg.body.user.id
 
   } else id = msg.body.event.user
-  // get the name from databasesss
   request({
     url: "https://slack.com/api/users.info?token=" + SLACK_ACCESS_TOKEN + "&user=" + id,
     json: true
   }, function (error, response, body) {
-    console.log("sssss" + body.user.profile.email)
     if (flag == 1) {
-      console.log("body.user.profile.email" + body.user.profile.email)
       callback(body.user.profile.email)
     } else
       msg.say(responseText + " " + body.user.name + "! How can I help you " + "?")
@@ -525,11 +522,9 @@ app.post('/uploaded_sick_report', (req, res) => {
  * 
  */
 slapp.action('preDefinedHelp', 'Show_stats', (msg, value) => {
-  // var stringfy = JSON.stringify(msg);
-  // console.log(""+JSON.parse(msg))
-  var email = SendWelcomeResponse(msg, "", 1, function (email) {
-    console.log("SHOW stats listenerr" + email)
 
+  var email = SendWelcomeResponse(msg, "", 1, function (email) {
+    employee.showEmployeeStats(email, msg)
   })
   //console.log(msg.body.user.id)
   // console.log(JSON.stringify(msg))
