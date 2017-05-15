d11b8e16b5b9970ae2b0e7cb99d676933db7a9c8
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-05-15 13:02:41 +0300

    add new fields

--- server.js
+++ server.js
@@ -521,7 +521,7 @@ app.post('/uploaded_sick_report', (req, res) => {
  */
 slapp.action('preDefinedHelp', 'Show_stats', (msg, value) => {
  // var stringfy = JSON.stringify(msg);
- console.log(""+JSON.parse(msg))
+// console.log(""+JSON.parse(msg))
   var email=SendWelcomeResponse(JSON.parse(msg),"",1)
   console.log("SHOW stats listenerr" + email)
 })
