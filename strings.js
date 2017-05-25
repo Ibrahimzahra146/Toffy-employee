f4e0ad16f255c96e6d2909ed341133e41470eb7c
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-05-25 16:00:38 +0300

    help order

--- strings.js
+++ strings.js
@@ -324,10 +324,13 @@ module.exports.sendVacationToManagerFunction = function sendVacationToManagerFun
     }
     var stringfy = JSON.stringify(messageBody)
     stringfy = stringfy.replace(/\\/, "")
-    
+
     stringfy = stringfy.replace(/\\/, "")
     stringfy = stringfy.replace(/}\"/, "}")
     stringfy = stringfy.replace(/\"\{/, "{")
+    stringfy = stringfy.replace(/\\/, "")
+
+    stringfy = stringfy.replace(/\\/, "")
     // stringfy = JSON.parse(stringfy)
 
     console.log(":::::::::messageBody" + stringfy)
