077bdd19b7eb56def3701856aaef6c8d44484215
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-05-28 11:39:50 +0300

    manager approval order

--- toffyHelper.js
+++ toffyHelper.js
@@ -186,7 +186,7 @@ module.exports.sendVacationToManager = function sendVacationToManager(startDate,
                 approvarType = managerApproval[i].type
                 managerEmail = emailFromId.replace(/\"/, "")
                 managerEmail = managerEmail.replace(/\"/, "")
-                messageGenerator.generateManagerApprovelsSection(managerApproval, managerEmail, function (managerApprovalMessage) {
+               // messageGenerator.generateManagerApprovelsSection(managerApproval, managerEmail, function (managerApprovalMessage) {
                     messageGenerator.generateYourActionSection(managerApproval, managerEmail, function (YourActionMessage) {
                         request({
                             url: 'http://' + IP + '/api/v1/toffy/get-record', //URL to hitDs
@@ -208,7 +208,7 @@ module.exports.sendVacationToManager = function sendVacationToManager(startDate,
                                     var timeststamp = new Date().getTime()
                                     //change 2
                                     message12 = stringFile.Slack_Channel_Function(jsonResponse.managerChannelId, jsonResponse.slackUserId, jsonResponse.teamId);
-                                    messageBody = stringFile.sendVacationToManagerFunction(comment, ImageUrl, userEmail, startDate, workingDays, endDate, type, approver2State, vacationId, approvalId, managerEmail, managerApprovalMessage,YourActionMessage);
+                                    messageBody = stringFile.sendVacationToManagerFunction(comment, ImageUrl, userEmail, startDate, workingDays, endDate, type, approver2State, vacationId, approvalId, managerEmail, "",YourActionMessage);
 
 
                                 } else if (approvarType == "HR") {
@@ -261,7 +261,7 @@ module.exports.sendVacationToManager = function sendVacationToManager(startDate,
                     })
 
                 })
-            })
+          //  })
 
             setTimeout(callback, 4000);
 
