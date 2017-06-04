945a81d000fbd210bc496eece8d67caa74475f75
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-06-04 14:55:53 +0300

    string file

--- toffyHelper.js
+++ toffyHelper.js
@@ -332,6 +332,7 @@ module.exports.sendVacationPostRequest = function sendVacationPostRequest(from, 
             body: vacationBody
             //Set the body as a stringcc
         }, function (error, response, body) {
+            console.log("sendVacationPostRequest"+ response.statusCode)
             var vacationId = (JSON.parse(body)).id;
             var managerApproval = (JSON.parse(body)).managerApproval
             callback(vacationId, managerApproval);
