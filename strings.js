a38878ab4840e795814e37d60f1072a580590640
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-06-05 10:47:22 +0300

    messageFB

--- strings.js
+++ strings.js
@@ -405,19 +405,28 @@ module.exports.Slack_Channel_Function = function Slack_Channel_Function(managerC
  *****************/
 module.exports.sendVacationToManagerFunction = function sendVacationToManagerFunction(comment, ImageUrl, userEmail, startDate, workingDays, endDate, type, approver2State, vacationId, approvalId, managerEmail, managerApprovalMessage, YourActionMessage) {
     console.log("sendVacationToManagerFunction" + managerApprovalMessage)
-
+    var reject_with_comment_button = {
+        "name": "reject_with_comment",
+        "text": "Reject with comment",
+        "style": "danger",
+        "type": "button",
+        "value": userEmail + ";" + vacationId + ";" + approvalId + ";" + managerEmail + ";employee" + ";" + startDate + ";" + endDate + ";" + type + ";" + workingDays + ";" + ImageUrl + ";" + "Pending" + ";" + "Pending" + ";" + "Pending"
+    }
     //  managerApprovalMessage = JSON.parse(managerApprovalMessage)
 
     var dont_detuct_button = stringFile.dont_detuct_button_Function(userEmail, vacationId, approvalId, managerEmail, startDate, endDate, type, workingDays, ImageUrl);
     var actions_based_on_type = dont_detuct_button
-    if (type == "sick")
+    if (type == "sick") {
+
+        reject_with_comment_button = ""
         actions_based_on_type = {
             "name": "accept_with_report",
             "text": "Accept with report",
-           
+
             "type": "button",
             "value": userEmail + ";" + vacationId + ";" + approvalId + ";" + managerEmail + ";employee" + ";" + startDate + ";" + endDate + ";" + type + ";" + workingDays + ";" + ImageUrl
         }
+    }
     var commentFieldInManagerMessage = stringFile.commentFieldInManagerMessageFunction(comment);
     var messageBody = {
         "text": "This folk has pending time off request:",
@@ -464,21 +473,15 @@ module.exports.sendVacationToManagerFunction = function sendVacationToManagerFun
                         "style": "primary",
                         "type": "button",
                         "value": userEmail + ";" + vacationId + ";" + approvalId + ";" + managerEmail + ";employee" + ";" + startDate + ";" + endDate + ";" + type + ";" + workingDays + ";" + ImageUrl + ";" + "Pending" + ";" + "Pending" + ";" + "Pending"
-                    },
+                    }, actions_based_on_type,
                     {
                         "name": "reject",
                         "text": "Reject",
                         "style": "danger",
                         "type": "button",
                         "value": userEmail + ";" + vacationId + ";" + approvalId + ";" + managerEmail + ";employee" + ";" + startDate + ";" + endDate + ";" + type + ";" + workingDays + ";" + ImageUrl + ";" + "Pending" + ";" + "Pending" + ";" + "Pending"
-                    },
-                    {
-                        "name": "reject_with_comment",
-                        "text": "Reject with comment",
-                        "style": "danger",
-                        "type": "button",
-                        "value": userEmail + ";" + vacationId + ";" + approvalId + ";" + managerEmail + ";employee" + ";" + startDate + ";" + endDate + ";" + type + ";" + workingDays + ";" + ImageUrl + ";" + "Pending" + ";" + "Pending" + ";" + "Pending"
-                    }, actions_based_on_type,
+                    }, reject_with_comment_button,
+
                     {
                         "name": "check_state",
                         "text": ":arrows_counterclockwise:",
