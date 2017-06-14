420542d770e24a14e23ee4202e4979e4daafa5cc
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-06-14 09:43:09 +0300

    send to manager issue

--- messageSender/messageGenerator.js
+++ messageSender/messageGenerator.js
@@ -21,7 +21,7 @@ module.exports.generateManagerApprovelsSection = function generateManagerApprove
             flag == "false }"
         }
         var arr = managerApproval[i].managerEmail.toString().split("@")
-        if (managerApproval[i].managerEmail != managerEmail&&managerApproval[i].type!="HR") {
+        if (managerApproval[i].managerEmail != managerEmail && managerApproval[i].type != "HR" || managerApproval[i].managerEmail == managerEmail && managerApproval[i].type != "Manager") {
 
 
             messageBody = messageBody + "{" + "\"title\":" + "\"" + "Approver ( " + arr[0] + " )\"" + ",\"value\":" + "\"" + managerApproval[i].state + ":thinking_face:" + "\"" + ",\"short\":" + flag
