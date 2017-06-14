e9e56b33bb25baec3fd9e3a8dece915b4bc4cada
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-06-14 13:03:00 +0300

    send to manager issue

--- server.js
+++ server.js
@@ -158,6 +158,7 @@ function getMembersList(Id, msg) {
 //*********************************************
 var app = env.slapp.attachToExpress(env.express())
 env.slapp.message('(.*)', ['direct_message'], (msg, text, match1) => {
+  console.log("Recieved")
   env.generalMsg = msg
   if (msg.body.event.user == "U4EN9UDHV") {
 
