0ca1acbf1f2753bb08e6460f6109120e7766ad48
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-06-15 14:14:31 +0300

    send to manager issue

--- toffyHelper.js
+++ toffyHelper.js
@@ -338,6 +338,7 @@ module.exports.sendVacationPostRequest = function sendVacationPostRequest(from, 
         } else if (type == "WFH") {
             vacationType = "7"
         }
+        console.log("sendVacationPostRequest" + vacationType)
 
         var vacationBody = {
             "employee_id": Id,
@@ -363,6 +364,7 @@ module.exports.sendVacationPostRequest = function sendVacationPostRequest(from, 
             if (response.statusCode == 500) {
                 callback(1000, 1000, 100)
             } else {
+                console.log("JSON.stringify(managerApproval)" + JSON.stringify(managerApproval))
 
                 var vacationId = (JSON.parse(body)).id;
                 var managerApproval = (JSON.parse(body)).managerApproval
