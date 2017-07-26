143ac9a55e5a044f3b10a64d41352a6a2ff403f7
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-07-26 08:56:11 +0300

    show sick converted to personal

--- toffy.service/TimeOffService.js
+++ toffy.service/TimeOffService.js
@@ -100,10 +100,7 @@ module.exports.vacationWithLeave = function vacationWithLeave(msg, response, ema
             }
             else if (response.result.parameters.working_from_home && (response.result.parameters.time == undefined) && (response.result.parameters.time1 == undefined) && (response.result.parameters.date == undefined) && (response.result.parameters.date1 == undefined)) {
                 //msg.say("Please specify the date and/or time ")
-                time = response.result.parameters.time
-                time1 = response.result.parameters.time1
-                date = response.result.parameters.date;
-                date1 = response.result.parameters.date1;
+           
                 console.log("sick_synonyms3")
                 vacation_type1 = "WFH"
 
